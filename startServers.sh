57567b2a373198a9f4df9adeb569237c8a6ca8b4
l3iggs <john.f.grub@gmail.com>
2015-05-06 12:03:28 +0200

    add ssl cert generation to startup

--- startServers.sh
+++ startServers.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 [ "$START_MYSQL" = true ] && cd /usr && /usr/bin/mysqld_safe --datadir=/var/lib/mysql&
+[ "$REGENERATE_SSL_CERT" = true ] && /etc/httpd/conf/genSSLKey.sh
 [ "$START_APACHE" = true ] && apachectl start
 
 # hang out right here until the image is terminated
